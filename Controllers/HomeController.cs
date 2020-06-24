db71e6fff3aba93d9ccb0772cdcc048fc81f5ca8
SPHyrule <stphnpino1@aol.com>
2020-06-24 12:07:49 +0100

    Now getting a OK response.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 
@@ -11,19 +12,15 @@ namespace GitHubSearcher.Controllers
     public class HomeController : Controller
     {
         /*Private field: HttpClient is intended to be instantiated once and re-used throughout the life of an application. Instantiating an HttpClient class for every request will exhaust the number of sockets available under heavy loads. This will result in SocketException errors. Below is an example using HttpClient correctly.The recommended practice is to create a single, shared HttpClient instance throughout the application.*/
-        private static readonly HttpClient HttpClient;        
+        private static HttpClient HttpClient;        
 
         public List<User> UserData { get; private set; }
 
         //Constructor
-        static HomeController()
+        public HomeController()
         {
-            //HttpClient is the new and improved way of doing HTTP requests and posts.
-            WebRequestHandler handler = new WebRequestHandler()
-            {
-                UseProxy = true,
-            };
-            HttpClient = new HttpClient(handler);
+            
+            HttpClient = new HttpClient();
 
         }    
 
@@ -41,20 +38,25 @@ namespace GitHubSearcher.Controllers
             5) Display this Json result data to user in a Razor view page.
         */
         [HttpGet]
-        public async Task<ActionResult> GetUserData(string searchString)
+        public async Task<ActionResult> GetAsync(string searchString)
         {
-            string newurl = searchString;
-
+            
+            
+            string newUrl = "users/" + searchString;
+            
+                        
             if (HttpClient.BaseAddress == null)
             {   //Passing service base url
-                HttpClient.BaseAddress = new Uri("https://api.github.com/users/");
+                HttpClient.BaseAddress = new Uri("https://api.github.com/");
             }
-            HttpClient.DefaultRequestHeaders.Clear();
+            HttpClient.DefaultRequestHeaders.Accept.Clear();
             // You should set the version so that GitHub knows what API you area calling
-            HttpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            HttpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            HttpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("product", "1"));
 
-            //Sending request to find web api REST service resource GetAllEmployees using HttpClient.  Set this to the URL you want.
-            HttpResponseMessage response = await HttpClient.GetAsync(newurl);
+            //Sending request to find web api REST service resource Get all Users using HttpClient.  Set this to the URL you want.
+            HttpResponseMessage response = await HttpClient.GetAsync(newUrl);
+
 
             //Checking the response is successful or not which is sent using HttpClient  
             if (response.IsSuccessStatusCode)
@@ -69,6 +71,7 @@ namespace GitHubSearcher.Controllers
 
             //returning the users list to view  
             return View("Index", UserData);
+            
         }
 
         
